42d1332292ebb998e761dee863f5a343d77a6f2e
Yureka <yuka@yuka.dev>
2023-12-19 14:01:55 +0100

    tmp

--- pkgs/tools/networking/goflow2/1.x.nix
+++ pkgs/tools/networking/goflow2/1.x.nix
@@ -5,13 +5,13 @@
 
 buildGoModule rec {
   pname = "goflow2";
-  version = "1.3.5";
+  version = "1.3.4";
 
   src = fetchFromGitHub {
     owner = "netsampler";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-YGPeaJqszAw7vQ/WzUBK7g9sAz9p0+aeBfteG4ZcrLU=";
+    hash = "sha256-0E3iSO+ObaPhIUerF4y5UygJMSMJNTJwI6RqHunqrZ0=";
   };
 
   ldflags = [
@@ -20,7 +20,7 @@ buildGoModule rec {
     "-X=main.version=${version}"
   ];
 
-  vendorHash = "sha256-jWm/061alLKTzn53uKun1qj2TM77pjltQlZl1/dTd80=";
+  vendorHash = "sha256-tNrCsCKBoUsrCOlbI1FUoksWoI4jUiYLF+A8Fjfe9Qk=";
 
   meta = with lib; {
     description = "High performance sFlow/IPFIX/NetFlow Collector";
